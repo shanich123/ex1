46796bcc2ecb09e30e535a1b37e742e5a6fb6ded
shanicheskis <shani.cheskis@mail.huji.ac.il>
2021-04-19 11:57:50 +0300

    n

--- src/webdata/Main.java
+++ src/webdata/Main.java
@@ -1,10 +1,19 @@
 package webdata;
+import java.io.*;
+import java.io.FileReader;
+import java.util.*;
+import java.io.FileOutputStream;
 
 public class Main {
 
     public static void main(String[] args) {
         SlowIndexWriter a = new SlowIndexWriter();
         a.slowWrite("1000.txt", "try");
-        a.removeIndex("try");
+//        a.removeIndex("try");
+        String inputString = "Hello World!";
+        byte[] byteArrray = inputString.getBytes();
+
+
+
     }
 }
--- src/webdata/SlowIndexWriter.java
+++ src/webdata/SlowIndexWriter.java
@@ -5,6 +5,38 @@ import java.util.*;
 
 public class SlowIndexWriter {
 
+    private String greatestCommonPrefix(String a, String b) {
+        int minLength = Math.min(a.length(), b.length());
+        for (int i = 0; i < minLength; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return a.substring(0, i);
+            }
+        }
+        return a.substring(0, minLength);
+    }
+    private void sort_tokens (String str[], int count){
+        String temp;
+        //Sorting the strings
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = i + 1; j < count; j++) {
+                if (str[i].compareTo(str[j])>0)
+                {
+                    temp = str[i];
+                    str[i] = str[j];
+                    str[j] = temp;
+                }
+            }
+        }
+        int avarage = 0;
+        String prefix;
+        for (int i = 1; i < count; i++){
+            prefix = greatestCommonPrefix(str[i], str[i-1]);
+            avarage += prefix.length();
+        }
+        System.out.println("average prefix length: " + (avarage/count));
+    }
+
     /**
      * Given product review data, creates an on disk index
      * inputFile is the path to the file containing the review data
@@ -19,11 +51,14 @@ public class SlowIndexWriter {
         }
         File file=new File(inputFile);    //read raw data
         Hashtable<String, Integer> tokens = new Hashtable<String, Integer>();
-        Hashtable<Integer, String> my_dict = new Hashtable<Integer, String>();
+        Hashtable<Integer, String> reviews_dict = new Hashtable<Integer, String>();
+        Hashtable<Integer, String> products_ids = new Hashtable<Integer, String>();
         int numReviews = 0;
         int numTokensAll = 0;
         int numDiffTokens = 0;
         int averageLen = 0;
+        byte[] byteArrray;
+        String last_product_id = "";
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -36,12 +71,16 @@ public class SlowIndexWriter {
                 if (line.isEmpty()){
                     numReviews += 1;
                     String finalS = curProductId + "&" + curHelpfulness + "&" + curScore + "&" + curLength.toString();
-                    my_dict.put(numDiffTokens, finalS);
+                    reviews_dict.put(numDiffTokens, finalS);
                 }
                 else {
                     if (line.startsWith("product/productId:")){
                         String[] arrOfStr = line.split(":", 2);
                         curProductId = arrOfStr[1];
+                        if (!last_product_id.equals(curProductId)){
+                            byteArrray = curProductId.getBytes();
+                            last_product_id = curProductId;
+                        }
                     }
                     else if (line.startsWith("review/helpfulness:")){
                         String[] arrOfStr = line.split(":", 2);
@@ -58,14 +97,17 @@ public class SlowIndexWriter {
                         curLength = words.length;
                         numTokensAll += curLength;
                         for (String word : words) {
-                            Integer per = tokens.get(word);
-                            if (per != null){
-                                tokens.put(word, per+1);
-                            }
-                            else {  // if the word didnt exist in the hashtable before
-                                numDiffTokens += 1;
-                                tokens.put(word, 1);
-                                averageLen += word.length();
+                            if (!word.isEmpty()) {
+                                word = word.toLowerCase(Locale.ROOT);
+                                Integer per = tokens.get(word);
+                                if (per != null){
+                                    tokens.put(word, per+1);
+                                }
+                                else {  // if the word didnt exist in the hashtable before
+                                    numDiffTokens += 1;
+                                    tokens.put(word, 1);
+                                    averageLen += word.length();
+                                }
                             }
                         }
                     }
@@ -76,6 +118,14 @@ public class SlowIndexWriter {
             System.out.println("number of tokens with repeats: " + numTokensAll);
             System.out.println("number of different tokens: " + numDiffTokens);
             System.out.println("average token length: " + (averageLen/numDiffTokens));
+            String str[] = new String[numDiffTokens];
+            Set<String> keys1 = tokens.keySet();
+            int i = 0;
+            for(String key: keys1){
+                str[i] = key;
+                i += 1;
+            }
+            sort_tokens(str, numDiffTokens);
             int averageF = 0;
             Set<String> keys = tokens.keySet();
             for(String key: keys){
